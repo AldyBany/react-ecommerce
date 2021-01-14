ccccf34cf4c285c8018f3bfd2759927bb31b6085
Aldy Bany <64736339+AldyBany@users.noreply.github.com>
2021-01-14 20:52:05 +0200

    init

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router , Switch, Route} from 'react-router-dom'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
 
   let [counter, setCounter] = useState(0)
+
+  const fectData = () =>{
+    axios.get('https://jsonplaceholder.typicode.com/')
+    .then(res => {
+      // handle success
+      console.log(res);
+      return res
+    })
+    .catch(err =>{
+      // handle error
+      console.error(err);
+    })
+  }
+
+  
+  
   
   return (
     <>
@@ -19,6 +36,8 @@ function App() {
 
       <h4>{counter}</h4>
       <button onClick={()=>setCounter(counter + 1)}>Increment Counter</button>
+
+      <button onClick={()=>fectData}>Get Data</button>
      
     </>
   );
